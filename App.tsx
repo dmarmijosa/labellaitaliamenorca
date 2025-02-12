eb6f351094ee4a683d83461a5377f027b421756f
Danny Armijos <daniel2667@hotmail.es>
2025-02-12 22:50:31 +0100

    primer commit de internacionalizacion

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { I18nextProvider, useTranslation } from "react-i18next";
+import { StyleSheet, Text, View } from "react-native";
+import i18n from "./assets/i18n/i18n";
 
 export default function App() {
   return (
+    <I18nextProvider i18n={i18n}>
+      <Main />
+    </I18nextProvider>
+  );
+}
+
+const Main = () => {
+  const {t} = useTranslation();
+  return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{t('welcome_message')}</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
