638d86dd09f2cc68d3addc67a666781bc2ee355f
Danny Armijos <daniel2667@hotmail.es>
2025-02-15 23:06:27 +0100

    fin de navegación y registro de estilos

---
+++ assets/i18n/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// Importa tus archivos de traducción
+import en from './locales/en.json';
+import es from './locales/es.json';
+
+i18n
+    .use(initReactI18next)
+    .init({
+        compatibilityJSON: 'v4',
+        lng: 'es', // Idioma por defecto
+        fallbackLng: 'en', // Idioma de reserva
+        resources: {
+            en: { translation: en },
+            es: { translation: es },
+        },
+        interpolation: {
+            escapeValue: false, // React ya maneja el escape de valores
+        },
+    });
+
+export default i18n;
